5e199bf44b0009359b8ba5fbece3ac2628e3f669
Davi Souza <davissbf@gmail.com>
2020-10-26 01:16:18 -0300

    footer

---
+++ src/components/Cards.js
@@ -0,0 +1,52 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from 'react';
+import CardItem from './CardItem';
+import './Cards.css';
+
+function Card() {
+  return (
+    <div className="cards">
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-9.jpg"
+              text="Explore the hidden waterfull deep inside the Amazon jungle"
+              label='Adventure'
+              path="/services"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Inslands of Bali in a Private Cruise"
+              label='Luxury'
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="images/img-3.jpg"
+              text="Set sail in the Atlantic Ocean visiting Uncharted Waters"
+              label='Mystery'
+              path="/services"
+            />
+            <CardItem
+              src="images/img-4.jpg"
+              text="Experience Football on Top of thr Himilayam Mountains"
+              label='Adventure'
+              path="/services"
+            />
+            <CardItem
+              src="images/img-8.jpg"
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label='Adrenaline'
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Card;
